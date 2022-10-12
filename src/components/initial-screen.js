c9e19788a937d043e3d58571418b1b86d87aa49f
Julia Pereira <juliapereira.odc@gmail.com>
2022-10-12 19:36:14 -0300

    Create InitialScreen Component

---
+++ src/components/initial-screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function InitialScreen() {
+    return (
+      <h1>
+         Tempo agora em ...
+      </h1>
+    )
+      
+  }
+  
+  export default InitialScreen;

e5ab0fab851f1ec875de4565c1b86be7fe753985
Julia Pereira <juliapereira.odc@gmail.com>
2022-10-12 19:36:28 -0300

    Create InitialScreen Style

---
+++ src/components/initial-screen-style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #6c7a89;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Box = styled.div`
+  background-color: red;
+  width: 400px;
+  height: 500px;
+  border-radius: 20px;
+  padding: 10px;
+
+  h1 {
+    color: white;
+    display: flex;
+    justify-content: center;
+  }
+`;
+

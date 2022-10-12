1268b9f57f7c13c73e937f10f87cedf4bd25d81c
Julia Pereira <juliapereira.odc@gmail.com>
2022-10-12 19:36:40 -0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import InitialScreen from "./components/initial-screen";
+import { Box, Body } from "./components/initial-screen-style";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Body>
+      <Box>
+          <InitialScreen></InitialScreen>
+      </Box>
+    </Body>
+      
+    </>
   );
 }
 

9b730b9685dfb90978c252323133f4a5da6e2166
Julia Pereira <juliapereira.odc@gmail.com>
2022-11-19 13:30:57 -0300

    Ajuste HTTPS

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -11,7 +11,7 @@ function Form() {
     e.preventDefault();
 
     let response1 = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=e83ae355fb450341a30c6df3a60135d7`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=e83ae355fb450341a30c6df3a60135d7`
     );
     let response2 = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${response1.data[0].lat}&lon=${response1.data[0].lon}&appid=e83ae355fb450341a30c6df3a60135d7&units=metric`
